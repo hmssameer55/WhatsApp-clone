6c9bf95defa15b162e780f71fd697edb620236cf
hmssameer <sameerahmed@codeglo.com>
2023-09-24 14:04:48 +0530

    readme

---
+++ README.md
@@ -0,0 +1,24 @@
+This is a clone of Whatsapp, made with React Native and Firebase & Tailwind.
+
+## Installation
+
+npm install to install all the dependencies.
+    
+    ```bash
+
+npx expo start to run the app in development mode.
+
+    ```bash
+
+## Usage
+
+I have removed the firebase config file, so you will have to add your own firebase config file in the firebase.js file.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT]()
+
--- firebase/helper/index.js
+++ firebase/helper/index.js
@@ -7,15 +7,7 @@ import { initializeApp } from 'firebase/app'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export const FirebaseApp = () => {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyDNeUvTYbauQqv-vdDnr3GgHc463yiJ2U8',
-    authDomain: 'whatsapp-edba1.firebaseapp.com',
-    projectId: 'whatsapp-edba1',
-    storageBucket: 'whatsapp-edba1.appspot.com',
-    messagingSenderId: '809139649998',
-    appId: '1:809139649998:web:459365f4dc566325fc3f9f',
-    measurementId: 'G-W6SGK3W3FE'
-  }
+  const firebaseConfig = {}
 
   // Initialize Firebase
   return initializeApp(firebaseConfig)
